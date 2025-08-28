2814c30032576856c4c62202c84969b40c2b43fe
StefanDeMoor <stefan.demoor@student.hogent.be>
2025-08-28 20:42:07 +0200

    Desing SuccesDialog verbeteren

--- app/src/main/java/com/example/riseep3/ui/componenten/SuccessDialog.kt
+++ app/src/main/java/com/example/riseep3/ui/componenten/SuccessDialog.kt
@@ -1,5 +1,6 @@
 package com.example.riseep3.ui.componenten
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -7,7 +8,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -18,6 +18,9 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -40,7 +43,7 @@ fun SuccessDialog(onDismiss: () -> Unit) {
         onDismissRequest = {},
         confirmButton = {},
         title = {},
-        containerColor = MaterialTheme.colorScheme.onSurface,
+        containerColor = colorResource(R.color.light_screen_add),
         text = {
             Box(
                 modifier = Modifier
@@ -50,14 +53,16 @@ fun SuccessDialog(onDismiss: () -> Unit) {
             ) {
                 Surface(
                     shape = CircleShape,
-                    color = MaterialTheme.colorScheme.surface,
+                    color = colorResource(R.color.light_screen_add),
                     modifier = Modifier.size(80.dp),
                     tonalElevation = 8.dp,
-                    shadowElevation = 8.dp
+                    shadowElevation = 8.dp,
+                    border = BorderStroke(2.dp, Color.White)
                 ) {
                     Box(contentAlignment = Alignment.Center) {
                         Image(
                             painter = painterResource(id = R.drawable.check),
+                            colorFilter = ColorFilter.tint(Color.White),
                             contentDescription = "Success",
                             modifier = Modifier.size(40.dp)
                         )
